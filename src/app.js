aa799302440f0bea2e233a08d9152c54b14171be
amrilsyaifa <amrilsyaifa@gmail.com>
2021-04-06 23:17:59 +0700

    first

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const path = require("path");
 const bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const { router } = require('bull-board')
-const { sendNewEmail } = require("./queues/email-queue");
+// const { router } = require('bull-board')
+// const { sendNewEmail } = require("./queues/email-queue");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,17 +18,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // for bull admin
-app.use('/admin/queues', router)
+// app.use('/admin/queues', router)
 
 // post from api
-app.post("/send-email", async (req, res) => {
-    const { message, ...restBody } = req.body;
-    await sendNewEmail({
-        ...restBody,
-        html: `<p>${message}</p>`,
-    });
-    res.send({ status: "ok" });
-});
+// app.post("/send-email", async (req, res) => {
+//     const { message, ...restBody } = req.body;
+//     await sendNewEmail({
+//         ...restBody,
+//         html: `<p>${message}</p>`,
+//     });
+//     res.send({ status: "ok" });
+// });
 
 app.get("/test", async (req, res) => {
     res.send({ status: "ok" });
