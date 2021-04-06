03d2cf8ce65a477cbc2bca50784b2f630c9be6a6
amrilsyaifa <amrilsyaifa@gmail.com>
2021-04-06 23:30:20 +0700

    add redis port

--- src/queues/email-queue.js
+++ src/queues/email-queue.js
@@ -5,7 +5,8 @@ const { setQueues, BullAdapter } = require("bull-board");
 // https://optimalbits.github.io/bull
 
 const emailQueue = new Bull("email", {
-    redis: "redis://127.0.0.1:6379",
+  // redis: "redis://127.0.0.1:6379",
+  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
 });
 
 setQueues([new BullAdapter(emailQueue)]);
@@ -13,9 +14,9 @@ setQueues([new BullAdapter(emailQueue)]);
 emailQueue.process(emailProcess);
 
 const sendNewEmail = (data) => {
-    emailQueue.add(data, {
-        attempts: 5,
-    });
+  emailQueue.add(data, {
+    attempts: 5,
+  });
 };
 
 module.exports = { sendNewEmail };
