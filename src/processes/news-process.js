8d22116827b17ef406b9517e2272cc2606770496
firmanjs <fimzhakim@gmail.com>
2021-06-14 14:28:37 +0700

    feature PING-323,324

--- src/processes/news-process.js
+++ src/processes/news-process.js
@@ -4,11 +4,11 @@ const validateDomain = async (resp) => {
     const getDomain = await DomainPage.findOne({
       where: { domain_name: resp.request.host }
     })
-    let domain_id
-    let name
-    let info
-    let domain_image
-    let created_domain
+    let domain_id;
+    let name;
+    let info;
+    let domain_image;
+    let created_domain;
     if (getDomain) {
       domain_id = getDomain.dataValues.domain_page_id;
       name = getDomain.dataValues.domain_name;
@@ -46,8 +46,10 @@ const validateDomain = async (resp) => {
   }
 }
 
-const putMainFeed = async (id, name, logo, created, data) => {
+const putMainFeed = async (job, name, logo, created, data) => {
   const { putStream } = require('../services');
+  const { setPostScore } = require('../processes/domain-process')
+  const score = await setPostScore(job.user_id)
   try {
     const set = {
       post_type: 2,
@@ -59,10 +61,10 @@ const putMainFeed = async (id, name, logo, created, data) => {
         description: data.description,
         image: data.image,
         url: data.news_url,
-      }
+      },...score
     }
-    await putStream(id, set);
-    console.info(`updated main_feed:${id}`)
+    await putStream(job.id_feed, set);
+    console.info(`updated main_feed:${job.id_feed}`)
   } catch (error) {
     console.info(error)
   }
@@ -70,7 +72,6 @@ const putMainFeed = async (id, name, logo, created, data) => {
 
 const saveNewsLink = async (data, name, info, job, logo, created_domain) => {
   try {
-    const id_feed = job.id_feed
     const { NewsLink } = require("../databases/models");
     const findNewsLink = await NewsLink.findOne({
       where: { news_url: data.news_url }
@@ -97,10 +98,10 @@ const saveNewsLink = async (data, name, info, job, logo, created_domain) => {
         content: data
       }
 
-      await postToGetstream(activity);
-      await putMainFeed(id_feed, name, logo, created_domain, data);
-      await saveCounterPost(job.user_id)
-      message = 'created'
+      await postToGetstream(activity, job.user_id);
+      await saveCounterPost(job.user_id);
+      await putMainFeed(job, name, logo, created_domain, data);
+      message = 'news link created'
     }
 
     return message
@@ -127,6 +128,9 @@ const saveCounterPost = async (user_id) => {
     const findPostToday = await StatisticPost.count({
       where : { user_id, date }
     });
+    /*
+      @description if exist post today counter else creatae post counter
+    */
     if (findPostToday) {
       await StatisticPost.increment(
         { counter: +1 },
@@ -146,13 +150,23 @@ const newsJob = async (job, done) => {
     console.info('news job is working! with id ' + job.id);
     const axios = require('axios');
     const cheerio = require('cheerio');
+
+    /*
+      @description crawls data from url post getstream
+    */
     const crawls = await axios.get(job.data.body);
+    /*
+      @description validate domain if exist get data or empty create data domain to table domain
+    */
     const getDomain = await validateDomain(crawls);
     const domain_page_id = getDomain.domain_id;
     const name = getDomain.name;
     const info = getDomain.info;
     const logo = getDomain.domain_image;
     const created_domain = getDomain.created_domain;
+    /*
+      @description crawls attribut opengraph url domain
+    */
     const $ = cheerio.load(crawls.data);
     const site_name = $('meta[property="og:site_name"]').attr('content') || "";
     const title = $("title").text();
@@ -162,10 +176,16 @@ const newsJob = async (job, done) => {
     const keyword = $('meta[name="keywords"]').attr('content') || "";
     const author = $('meta[name="author"]').attr('content') || "";
 
+    /*
+      @description define data for post to getstream and newslink table
+    */
     const data = {
       domain_page_id, title, site_name, image, description, news_url, keyword, author
     };
 
+    /*
+      @description save news link and post to getstream
+    */
     const result = await saveNewsLink(data, name, info, job.data, logo, created_domain);
     console.info(result);
     done(null , result);
