cc290dcc208ccca03f451e08a362761602e510a6
firmanjs <fimzhakim@gmail.com>
2021-06-21 11:04:06 +0700

    update formula testing

--- src/processes/post-perfomance-process.js
+++ src/processes/post-perfomance-process.js
@@ -1,11 +1,44 @@
-const { postPerformanceScore } = require("../utils")
+const {
+  postScore, nonBpScoreWilsonScore, DurationScoreWilsonScore,
+  upDownScoreWilsonScore, upDownScore
+} = require("../utils")
+
+require('dotenv').config()
 
 const postPerformanceScoreProcess = () => {
   const WW_NON_BP = process.env.WW_NON_BP;
   const WW_D = process.env.WW_D;
   const WW_UP_DOWN = process.env.WW_UP_DOWN;
-  const result = postPerformanceScore(1, 1, WW_NON_BP,1, WW_D, 1, WW_UP_DOWN);
-  console.info(result)
+  const Z_NON_BP = process.env.Z_NONBP;
+  const EV_NON_BP = process.env.EV_NONBP_PERCENTAGE;
+  const Z_D = process.env.Z_D;
+  const EV_D_PERCENTAGE = process.env.EV_D_PERCENTAGE;
+  const Z_UPDOWN = process.env.Z_UPDOWN;
+  const EV_UPDOWN_PERCENTAGE = process.env.EV_UPDOWN_PERCENTAGE;
+  const W_DOWN = process.env.W_DOWN;
+  const W_N = process.env.W_N;
+  const bp = 2 // dummy data
+  const impression = 10 // dummy data
+  const duration = 10 //dummy data
+  const upvote = 11;
+  const downvote = 11;
+  /*
+    @description block point get from table post_blocked
+  */
+  const wsnonbp = nonBpScoreWilsonScore(bp, impression, Z_NON_BP, EV_NON_BP);
+  /*
+    @description
+  */
+  const wsd = DurationScoreWilsonScore(impression, duration, Z_D, EV_D_PERCENTAGE);
+  /*
+    @description upvote = Sum of Upvote-Points of a Post
+    downvote = Sum of Downvote-Points of a Post
+  */
+  const sUpDown = upDownScore(impression, upvote, downvote, W_DOWN, W_N);
+  const wsupdown = upDownScoreWilsonScore(impression, sUpDown, Z_UPDOWN, EV_UPDOWN_PERCENTAGE);
+  const result = postScore(impression, wsnonbp, WW_NON_BP, wsd, WW_D, wsupdown, WW_UP_DOWN);
+
+  console.info(result);
 }
 
 postPerformanceScoreProcess();
--- src/utils/formula.js
+++ src/utils/formula.js
@@ -15,14 +15,14 @@ const postScore = (impr, wsNonBp, wwNonBp, wsD, wwD, wsUpdown, wwUpdown) => {
   let result_p_pref;
 
   if (impr < 5) {
-    result_p_pref = p_pref
+    result_p_pref = p_pref;
   } else if (impr < 50) {
     result_p_pref = p_pref * (wsNonBp ** wwNonBp) * (wsD ** wwD);
   } else {
     result_p_pref = p_pref * (wsNonBp ** ww_nonbp) * (ws_d ** ww_d) * (wsUpdown ** wwUpdown);
   }
 
-  return result_p_pref
+  return result_p_pref;
 }
 
 /*
@@ -36,9 +36,9 @@ const weightPostLongComments = (longC, impr, wlongC) => {
 /*
   @description formula for variable WS_updown
 */
-const upDownScoreWilsonScore = (impr, sUpDwon, zUpDown, evUpDown) => {
+const upDownScoreWilsonScore = (impr, sUpDown, zUpDown, evUpDown) => {
   const evUpDownPercentage = evUpDown / 100;
-  const result = ((sUpDwon + (zUpDown ** 2 / (2 * impr))) / (1 + (zUpDown ** 2) / impr)) / evUpDownPercentage;
+  const result = ((sUpDown + (zUpDown ** 2 / (2 * impr))) / (1 + (zUpDown ** 2) / impr)) / evUpDownPercentage;
 
   return result;
 }
@@ -67,7 +67,7 @@ const nonBpScoreWilsonScore = (bp, impr, zNonbp, evNonBp) => {
 }
 
 /*
-  @description formula for variable sUpDwon
+  @description formula for variable sUpDown
 */
 const upDownScore = (impr, upvote, downvote, wDown, wN) => {
   return ((-impr * wDown) + upvote + (downvote * wDown) + wN * (impr - upvote - downvote)) / (impr * (1 - wDown));
