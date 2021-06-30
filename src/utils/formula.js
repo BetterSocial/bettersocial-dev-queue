28640c3d071fc9b7207ccb0d8660ccf7cbb5184d
firmanjs <fimzhakim@gmail.com>
2021-06-30 14:25:30 +0700

    updated formula

--- src/utils/formula.js
+++ src/utils/formula.js
@@ -42,6 +42,7 @@ const upDownScoreWilsonScore = (impr, sUpDown, zUpDown, evUpDown) => {
 
   return result;
 }
+
 /*
   @description formula for variable WS_D
 */
@@ -91,6 +92,170 @@ const postPerformanceScore = (pPerf, pLongC) => {
 */
 const dBench = (dur_min, dur_marg, W) => {
   return dur_min + dur_marg * W
+}
+
+/*
+  @description formula for variable u
+*/
+const userScore = (u1, y, wy) => {
+  return u1 * y ** wy
+}
+
+/*
+  @description formula for variable T_t
+*/
+const finalScorePost = (userScore, weightUserScroe, p1, weightp1, p2, weightp2, p3, weightp3, prev, weightprev) => {
+  return userScore ** weightUserScroe * p1 ** weightp1 * p2 ** weightp2 * p3 ** weightp3 * prev ** weightprev;
+}
+
+/*
+  @description formula for variable f
+*/
+const followerScore = (followersCount) => {
+  return (followersCount / 150) ** (0.05);
+}
+
+/*
+  @description formula for variable b
+*/
+const blockedPerPostImpression = (blockpointsPerImpression) => {
+  return 2 / (1 + 200 ** (blockpointsPerImpression ** 0.4 - 1));
+}
+
+/*
+  @description formula for variable BPpImpr_un
+*/
+const blockpointsPerImpression = (totalBlocks, impr, bpImprGlobal) => {
+  if (impr == 0) {
+    return 0;
+  } else {
+    return (totalBlocks / impr) / bpImprGlobal;
+  }
+}
+
+/*
+  @description formula for variable r
+*/
+const averagePostScore = (postPerformanceScore, countPosts) => {
+  if (countPosts == 0) {
+    return 1;
+  } else {
+    return (postPerformanceScore + (10 - min(10, countPosts))) / 10;
+  }
+}
+
+/*
+  @description formula for variable q
+*/
+const multiplicationFromQualityCriteriaScore = (wEdu, eduEmail, wEmail, wTwitter, followerTwitter, email, wUserAtt) => {
+  let verifiedEdu
+  let twitter
+  if (eduEmail) {
+    verifiedEdu = wEdu ** 1;
+  } else {
+    verifiedEdu = wEdu ** 0;
+  }
+  const veridiedEmail = wEmail ** (min(3, email) ** 0.25);
+  if (followerTwitter > 200) {
+    twitter = wTwitter ** 1;
+  } else {
+    twitter = wTwitter ** 0;
+  }
+
+  const result = verifiedEdu * veridiedEmail * twitter * wUserAtt;
+  return result;
+}
+
+/*
+  @description formula for variable u1
+*/
+const userScoreWithoutFollower = (f, wF, b, wB, r, wR, q, wQ, a, wA) => {
+  return (f ** wF) * (b ** wB) * (r ** wR) * (q ** wQ) * (a ** wA);
+}
+
+/*
+  @description formula for variable y
+*/
+const followersQuality = (userScoreWithoutFollowerScore, followersCount) => {
+  //TODO pastikan input nya
+  return userScoreWithoutFollowerScore / followersCount;
+}
+
+/*
+  @description formula for variable p1
+*/
+const applyMultipliesToTotalScore = (wTopic, topicFollowed, wFollow, wDegree, wLinkDomain, userFollowAuthor, followAuthorFollower, linkPost) => {
+  const constant = 0.5;
+  const followedTopic = wTopic ** (topicFollowed ** constant);
+  let calculate
+  if (userFollowAuthor) {
+    calculate = followedTopic * wFollow;
+  }
+  else if (followAuthorFollower == 1) {
+    calculate = followedTopic * wDegree;
+  }
+  else {
+    calculate = followedTopic * 1;
+  }
+  const result = calculate * wLinkDomain ** linkPost;
+  return result;
+}
+
+/*
+  @description formula for variable p2
+*/
+const scoreBasedPostCharacteristics = (rec, wRec, att, wAtt, d, wD, p, wP, postLink) => {
+  const formula = rec ** wRec * att ** wAtt;
+  let result
+  if (postLink) {
+    result = formula * d ** wD;
+  } else {
+    result = formula * 1 * p ** wP;
+  }
+  return result;
+}
+
+/*
+  @description formula for variable p_perv
+*/
+const previousInteractionScore = (prevInteract, prevD, prevUc, prevPre) => {
+  if (prevInteract == 'seen') {
+    return prevPre;
+  } else if (prevInteract == 'downvote') {
+    return prevD;
+  } else if (prevInteract == 'upvote' || prevInteract == 'comment') {
+    return prevUc;
+  } else {
+    return 1;   //none interaction
+  }
+}
+
+/*
+  @description formula for variable Rec
+*/
+const previousInteractionScore = (ageOfPost, expirationSetting) => {
+  if (expirationSetting == 1) {
+    return 1 - 0.007 * ageOfPost;
+  } else if (expirationSetting == 7) {
+    return 1.3 - 0.4 * ageOfPost ** 0.15;
+  } else if (expirationSetting == 30) {
+    return 0.95 - 0.225 * ageOfPost ** 0.215;
+  } else if (expirationSetting == 'forever') {
+    return max(0.02, 0.95 - 0.225 * ageOfPost ** 0.215);
+  }
+}
+
+/*
+  @description formula for variable t
+*/
+const previousInteractionScore = (expirationSetting, postDateTime, nowDateTime) => {
+  diffDays = (Date.parse(nowDateTime) - Date.parse(postDateTime)) / 60 / 60 / 24;
+
+  if(expirationSetting == "forever") {
+    return max(1, diffDays);
+  } else {
+    return min(expirationSetting, max(1, diffDays));
+  }
 }
 
 module.exports = {
