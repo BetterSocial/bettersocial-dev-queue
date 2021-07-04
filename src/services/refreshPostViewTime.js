5fd68959ac9c02b7031581cf5e00fd6e98faa143
firmanjs <fimzhakim@gmail.com>
2021-07-04 22:26:19 +0700

    update response

--- src/services/refreshPostViewTime.js
+++ src/services/refreshPostViewTime.js
@@ -1,20 +1,12 @@
 const refreshPostViewTime = async (req, res) => {
   try {
-    const db = require('../databases/models')
+    const { successResponse, errorResponse } = require('../utils');
+    const db = require('../databases/models');
     const query = `REFRESH MATERIALIZED VIEW vw_post_time`
     await db.sequelize.query(query);
-    return res.status(200).json({
-      code: 200,
-      status: "ok",
-      data: "refresh data sucesfully",
-    });
+    return successResponse(res, "refresh data sucesfully", []);
   } catch (error) {
-    return res.status(500).json({
-      code: 500,
-      data: null,
-      message: "Internal server error",
-      error: error.toString(),
-    });
+    return errorResponse(res, error.toString(), 500);
   }
 }
 
--- src/utils/response.js
+++ src/utils/response.js
@@ -2,6 +2,7 @@ const notFoundHandler = (req, res) => {
   const msg = `Route : ${req.url} Not found.`
   const err = new Error(msg)
   res.status(404).json({
+    code: 404,
     error: err.toString(),
     status: 404,
     msg,
@@ -11,20 +12,23 @@ const notFoundHandler = (req, res) => {
 const errorHandler = (error, res) => {
   if (!error.statusCode) error.statusCode = 500
   res.status(error.statusCode).json({
+    code: error.statusCode || 500,
     error: error.toString(),
     status: error.statusCode,
-    msg: error.toString()
+    msg: error.toString(),
   })
 }
 
 const successResponse = (res, msg, data) => res.status(200).json({
-  message: `${msg} data successfull`,
+  code: 200,
+  message: `${msg}`,
   status: 'success',
   data
 })
 
 const errorResponse = (res, msg, code) => {
   const message = {
+    code,
     message: `Error. ${msg}`,
     status: 'something wrong',
     data: []
@@ -41,6 +45,7 @@ const errorResponse = (res, msg, code) => {
 
 const customResponse = (res, code, msg, data) => {
   res.status(code).json({
+    code,
     message: msg,
     data
   })
