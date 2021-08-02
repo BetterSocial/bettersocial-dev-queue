75dc7cf97b0d2d289fdaff8e61eebd99b31c6823
usup <usup@solusiteknologi.co.id>
2021-08-02 23:14:44 +0700

    add follow to getstream

---
+++ src/processes/follow-location-process.js
@@ -0,0 +1,53 @@
+const { PostViewTime } = require("../databases/models");
+
+const followLocation = async (job, done) => {
+  try {
+    console.info("running job follow location! with id " + job.id);
+    /*
+      @description job follow location to getstream
+    */
+
+    console.log(job.data);
+
+    done(null, job.data);
+  } catch (error) {
+    done(null, error);
+  }
+};
+
+const followUser = async (job, done) => {
+  try {
+    console.info("running job follow user! with id " + job.id);
+    /*
+      @description job follow user to getstream
+    */
+
+    console.log(job.data);
+
+    done(null, job.data);
+  } catch (error) {
+    done(null, error);
+  }
+};
+
+const followTopic = async (job, done) => {
+  try {
+    console.info("running job follow topic! with id " + job.id);
+    /*
+      @description job follow user to getstream
+      here logic to follow topic
+    */
+
+    console.log(job.data);
+
+    done(null, job.data);
+  } catch (error) {
+    done(null, error);
+  }
+};
+
+module.exports = {
+  followLocation,
+  followUser,
+  followTopic,
+};
--- src/queues/job-queue.js
+++ src/queues/job-queue.js
@@ -1,24 +1,57 @@
-const { newsJob } = require('../processes/news-process');
-const { createPostTime } = require('../processes/post-time-process');
-const { handlerFailure, handlerCompleted, handlerStalled } = require('./handler');
+const { newsJob } = require("../processes/news-process");
+const { createPostTime } = require("../processes/post-time-process");
+const {
+  followLocation,
+  followTopic,
+  followUser,
+} = require("../processes/follow-location-process");
+const {
+  handlerFailure,
+  handlerCompleted,
+  handlerStalled,
+} = require("./handler");
 
-const { newsQueue, postTimeQueue  } = require('../config')
+const {
+  newsQueue,
+  postTimeQueue,
+  locationQueue,
+  followTopicQueue,
+  followUserQueue,
+} = require("../config");
 
 /*
   @description initial all job queue
 */
 const initQueue = () => {
-  console.info('newsQueue job is working!');
+  console.info("newsQueue job is working!");
   newsQueue.process(newsJob);
-  newsQueue.on('failed', handlerFailure);
-  newsQueue.on('completed', handlerCompleted);
-  newsQueue.on('stalled', handlerStalled);
+  newsQueue.on("failed", handlerFailure);
+  newsQueue.on("completed", handlerCompleted);
+  newsQueue.on("stalled", handlerStalled);
 
-  console.info('postTimeQueue job is working!');
+  console.info("postTimeQueue job is working!");
   postTimeQueue.process(createPostTime);
-  postTimeQueue.on('failed', handlerFailure);
-  postTimeQueue.on('completed', handlerCompleted);
-  postTimeQueue.on('stalled', handlerStalled);
-}
+  postTimeQueue.on("failed", handlerFailure);
+  postTimeQueue.on("completed", handlerCompleted);
+  postTimeQueue.on("stalled", handlerStalled);
+
+  console.info("Follow location job is working!");
+  locationQueue.process(followLocation);
+  locationQueue.on("failed", handlerFailure);
+  locationQueue.on("completed", handlerCompleted);
+  locationQueue.on("stalled", handlerStalled);
+
+  console.log("Follow user job is working!");
+  followUserQueue.process(followUser);
+  followUserQueue.on("failed", handlerFailure);
+  followUserQueue.on("completed", handlerCompleted);
+  followUserQueue.on("stalled", handlerStalled);
+
+  console.log("Follow topic job is working!");
+  followTopicQueue.process(followTopic);
+  followTopicQueue.on("failed", handlerFailure);
+  followTopicQueue.on("completed", handlerCompleted);
+  followTopicQueue.on("stalled", handlerStalled);
+};
 
 initQueue();
