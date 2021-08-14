d4e72b4d32f99b7feed1152af4868247a34a4737
usup <usup@solusiteknologi.co.id>
2021-08-14 23:28:35 +0700

    fixing add member to channel

--- src/processes/chat-process.js
+++ src/processes/chat-process.js
@@ -1,9 +1,67 @@
+// const stream = require("getstream");
+// const streamChat = require("stream-chat");
+
+const StreamChat = require("stream-chat").StreamChat;
+
 const addMemberToChannel = async (job, done) => {
   try {
     console.info("running job add member to channel " + job.id);
     console.log(job.data);
+    let data = job.data;
+    let { locations, user_id } = data;
+    let userId = user_id.toString();
+    const serverClient = StreamChat.getInstance(
+      process.env.API_KEY,
+      process.env.SECRET
+    );
+    // const updateResponse = await serverClient.upsertUser({
+    //   id: userID,
+    //   role: "user",
+    // });
+    // console.log(updateResponse);
+    locations.map(async (item) => {
+      console.log(item);
+      const token = serverClient.createToken(userId);
+      console.log(token);
+      const channelId = item;
+      const members = [];
+      members.push(user_id);
+      const channel = serverClient.channel("messaging", channelId, {
+        name: `Awesome channel about ${channelId}`,
+        created_by_id: "e554d0ac-81cc-4139-9939-11de565cda27",
+      });
+      await channel.create();
+
+      await channel.addMembers(members, {
+        text: "Tommaso joined the channel.",
+        user_id: user_id,
+      });
+    });
+
+    // you can still use new StreamChat('api_key', 'api_secret');
+
+    // generate a token for the user with id 'john'
+
+    // let members = users.map((item) => item.user_id);
+    // members.push(myProfile.user_id);
+    // let channelName = users.map((item) => {
+    //   return item.username;
+    // });
+    // channelName.push(myProfile.username);
+    // const client = await streamChat.StreamChat.getInstance(
+    //   process.env.API_KEY,
+    //   process.env.SECRET
+    // );
+    // console.log(client);
+    // const client = stream.connect(process.env.API_KEY, process.env.SECRET);
+    // const channelChat = await clientChat.channel("messaging", {
+    //   name: channelName,
+    //   members: members,
+    // });
+    // await channelChat.watch();
     done(null, job.data);
   } catch (error) {
+    console.log(error);
     done(null, error);
   }
 };
