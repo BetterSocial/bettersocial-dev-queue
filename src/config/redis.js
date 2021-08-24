8c2d3f967d3c883bb88c898d19496ca815ff99f4
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-08-24 11:24:03 +0800

    change config redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,9 @@
 const Bull = require("bull");
 
-const newsQueue = new Bull("newsQueue", {
-  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
-});
+// const newsQueue = new Bull("newsQueue", {
+//   redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
+// });
+const newsQueue = new Bull("newsQueue", String(process.env.REDIS_URL));
 newsQueue.on("error", (err) => {
   console.log("newsQueue error : ", err);
   console.log("redis url ", process.env.REDIS_URL);
