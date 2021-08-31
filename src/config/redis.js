aa48892ce2717122c93c7ce54cc05ce4b05a174b
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-08-31 11:31:05 +0800

    update config connection

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,40 +1,44 @@
 const Bull = require("bull");
 
-// const newsQueue = new Bull("newsQueue", {
-//   redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
-// });
-const newsQueue = new Bull("newsQueue", process.env.REDIS_URL, {
+const newsQueue = new Bull("newsQueue", process.env.REDIS_TLS_URL, {
   redis: { tls: { rejectUnauthorized: false } },
 });
-const testQueue = new Bull("testQueue", String(process.env.REDIS_URL), {
+const testQueue = new Bull("testQueue", String(process.env.REDIS_TLS_URL), {
   redis: { tls: { rejectUnauthorized: false } },
 });
 testQueue.on("error", (err) => {
   console.log("err test ", err);
 });
-
-// const emailQueue = new Bull("email", {
-//   redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
-// });
-
-const postTimeQueue = new Bull("addQueuePostTime", {
-  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
+const postTimeQueue = new Bull("addQueuePostTime", process.env.REDIS_TLS_URL, {
+  redis: { tls: { rejectUnauthorized: false } },
 });
 
-const locationQueue = new Bull("followLocationQueue", {
-  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
-});
+const locationQueue = new Bull(
+  "followLocationQueue",
+  process.env.REDIS_TLS_URL,
+  {
+    redis: { tls: { rejectUnauthorized: false } },
+  }
+);
 
-const followUserQueue = new Bull("followUserQueue", {
-  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
+const followUserQueue = new Bull("followUserQueue", process.env.REDIS_TLS_URL, {
+  redis: { tls: { rejectUnauthorized: false } },
 });
-const followTopicQueue = new Bull("followTopicQueue", {
-  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
-});
+const followTopicQueue = new Bull(
+  "followTopicQueue",
+  process.env.REDIS_TLS_URL,
+  {
+    redis: { tls: { rejectUnauthorized: false } },
+  }
+);
 
-const addUserToChannelQueue = new Bull("addUserToChannelQueue", {
-  redis: process.env.REDIS_URL || process.env.REDIS_TLS_URL,
-});
+const addUserToChannelQueue = new Bull(
+  "addUserToChannelQueue",
+  process.env.REDIS_TLS_URL,
+  {
+    redis: { tls: { rejectUnauthorized: false } },
+  }
+);
 
 module.exports = {
   newsQueue,
