b2756a6bfc0f33ad50d4becc8b040fa4aefa3168
usupsuparma <usup@solusiteknologi.co.id>
2021-12-14 21:59:20 +0700

    create log

--- src/webhook/setNews.js
+++ src/webhook/setNews.js
@@ -5,20 +5,18 @@ const createQueueNews = async (req, res) => {
   // const user_id = req.body.actor_id || null
   // const topics = req.body.topics || null
   // const duration_feed = +req.body.duration_feed || null
-  const test = req.body[0]?.new[0];
   const bodyData = req.body[0]?.new[0]?.message;
   const id_feed = req.body[0]?.new[0]?.id || false;
   const user_id = req.body[0]?.new[0]?.actor?.id || false;
   const topics = req.body[0]?.new[0]?.topics || [];
   const duration_feed = req.body[0]?.new[0]?.duration_feed || "";
-  console.log(test);
-
   const {
     checkIfValidURL,
     successResponse,
     errorResponse,
   } = require("../utils");
   if (bodyData) {
+    console.log('body data');
     try {
       const { v4: uuidv4 } = require("uuid");
       const { newsQueue } = require("../config");
@@ -30,6 +28,7 @@ const createQueueNews = async (req, res) => {
         removeOnComplete: true,
       };
       if (checkIfValidURL(bodyData)) {
+        console.log('validasi url berhasil');
         const getJob = await newsQueue.add(
           {
             body: checkIfValidURL(bodyData),
@@ -40,19 +39,23 @@ const createQueueNews = async (req, res) => {
           },
           options
         );
+        console.log(`success created news with job id : ${getJob.id}`);
         return successResponse(
           res,
           `success created news with job id : ${getJob.id}`,
           bodyData
         );
       } else {
-        throw new Error("url is invalid");
+        console.log('error validasi url');
+        return errorResponse(res, 'url is invalid', 400);
+        // throw new Error("url is invalid");
       }
     } catch (error) {
       console.log(error);
       return errorResponse(res, error.toString(), 500);
     }
   } else {
+    console.log('sukses tanpa data');
     return successResponse(res, "ok", "Job running");
   }
 };
