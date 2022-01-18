ac69132cb9942ac7d85b6b272358d2d7c7b62805
usupsuparma <usup@solusiteknologi.co.id>
2022-01-18 23:39:05 +0700

    fixing create queue for prepopulated dm

---
+++ src/processes/prepopulate-dm-process.js
@@ -0,0 +1,27 @@
+const prepopulated = require("../services/chat/prepopulated");
+const UserService = require("../services/postgres/UserService");
+
+const prepopulatedDm = async (job, done) => {
+
+  try {
+    let data = job.data;
+    let { id, ids } = data;
+    let userService = new UserService();
+    let userAdmin = await userService.getUserAdmin(process.env.USERNAME_ADMIN);
+    let idAdmin = userAdmin.user_id;
+    ids = ids.filter((element, i, ids) => {
+      return (element !== idAdmin);
+    })
+    ids.push(idAdmin);
+    let users = await userService.getUsersByIds(ids)
+    const pre = await prepopulated(id, users);
+    done(null, pre);
+  } catch (error) {
+    console.log(error);
+    done(null, error);
+  }
+}
+
+module.exports = {
+  prepopulatedDm
+}
