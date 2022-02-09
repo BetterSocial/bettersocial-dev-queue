d9c0c81930d8a146bbaeba075d943544901b71af
usupsuparma <usup@solusiteknologi.co.id>
2022-02-09 15:57:39 +0700

    fixing register fixing register only use one queue

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,15 +2,12 @@ const Bull = require("bull");
 
 const connectRedis = process.env.REDIS_URL;
 
-const newsQueue = new Bull("newsQueue", connectRedis, { redis: { tls: { rejectUnauthorized: false } } });
-// const testQueue = new Bull("testQueue", process.env.REDIS_URL, {
-// redis: { tls: { rejectUnauthorized: false } },
-// }, {
-//   redis: { tls: { rejectUnauthorized: false } },
-// });
-// testQueue.on("error", (err) => {
-//   console.log("err test ", err);
-// });
+const newsQueue = new Bull("newsQueue",
+  connectRedis,
+  {
+    redis: { tls: { rejectUnauthorized: false } }
+  }
+);
 
 const postTimeQueue = new Bull(
   "addQueuePostTime",
@@ -20,62 +17,71 @@ const postTimeQueue = new Bull(
   }
 );
 
-const locationQueue = new Bull(
-  "followLocationQueue",
+// const locationQueue = new Bull(
+//   "followLocationQueue",
+//   connectRedis,
+//   {
+//     redis: { tls: { rejectUnauthorized: false } }
+//   }
+// );
+
+// const followUserQueue = new Bull(
+//   "followUserQueue",
+//   connectRedis,
+//   {
+//     redis: { tls: { rejectUnauthorized: false } }
+//   }
+// );
+
+// const followTopicQueue = new Bull(
+//   "followTopicQueue",
+//   connectRedis,
+//   {
+//     redis: { tls: { rejectUnauthorized: false } }
+//   }
+// );
+
+// const addUserToChannelQueue = new Bull(
+//   "addUserToChannelQueue",
+//   connectRedis,
+//   {
+//     redis: { tls: { rejectUnauthorized: false } }
+//   }
+// );
+
+// const addUserToTopicChannelQueue = new Bull(
+//   "addUserToTopicChannelQueue",
+//   connectRedis,
+//   {
+//     redis: { tls: { rejectUnauthorized: false } }
+//   }
+// );
+
+// const prepopulatedDmQueue = new Bull(
+//   "prepopulatedDmQueue",
+//   connectRedis,
+//   {
+//     redis: { tls: { rejectUnauthorized: false } }
+//   }
+// );
+
+const registerQueue = new Bull(
+  "registerQueue",
   connectRedis,
   {
     redis: { tls: { rejectUnauthorized: false } }
   }
 );
-
-const followUserQueue = new Bull(
-  "followUserQueue",
-  connectRedis,
-  {
-    redis: { tls: { rejectUnauthorized: false } }
-  }
-);
-
-const followTopicQueue = new Bull(
-  "followTopicQueue",
-  connectRedis,
-  {
-    redis: { tls: { rejectUnauthorized: false } }
-  }
-);
-
-const addUserToChannelQueue = new Bull(
-  "addUserToChannelQueue",
-  connectRedis,
-  {
-    redis: { tls: { rejectUnauthorized: false } }
-  }
-);
-
-const addUserToTopicChannelQueue = new Bull(
-  "addUserToTopicChannelQueue",
-  connectRedis,
-  {
-    redis: { tls: { rejectUnauthorized: false } }
-  }
-);
-
-const prepopulatedDmQueue = new Bull(
-  "prepopulatedDmQueue",
-  connectRedis,
-  {
-    redis: { tls: { rejectUnauthorized: false } }
-  }
-)
 
 module.exports = {
   newsQueue,
   postTimeQueue,
-  locationQueue,
-  followTopicQueue,
-  followUserQueue,
+  // locationQueue,
+  // followTopicQueue,
+  // followUserQueue,
   // testQueue,
-  addUserToChannelQueue,
-  addUserToTopicChannelQueue,
-  prepopulatedDmQueue,
+  // addUserToChannelQueue,
+  // addUserToTopicChannelQueue,
+  // prepopulatedDmQueue,
+  registerQueue,
 };
--- src/services/followUser.js
+++ src/services/followUser.js
@@ -5,9 +5,10 @@ const {
   UserFollowUser,
   UserFollowUserHistory } = require("../databases/models");
 const { v4: uuidv4 } = require("uuid");
+
 const followUser = async (token, userId, feedGroup, status = 1) => {
   const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
-  const user = client.feed("main_feed", client.userId, token);
+  const user = client.feed("main_feed", userId, token);
   if (status === 1) {
     return user.follow(feedGroup, userId);
   } else {
@@ -15,20 +16,19 @@ const followUser = async (token, userId, feedGroup, status = 1) => {
   }
 };
 
-const followUsers = async (token, userIds) => {
+const followUsers = async (userId, userIds) => {
   try {
     const result = await sequelize.transaction(async (t) => {
-      const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
+      // const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
       const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
       const userSevice = new UserService();
       let userAdmin = await userSevice.getUserAdmin(process.env.USERNAME_ADMIN);
       let id = userAdmin.user_id;
-      console.log('id admin', id);
       // User Follow User
       let followUser = {
         //   generate UUID
         follow_action_id: uuidv4(),
-        user_id_follower: client.userId,
+        user_id_follower: userId,
         user_id_followed: id,
       };
 
@@ -42,7 +42,7 @@ const followUsers = async (token, userIds) => {
       console.info(statusFollowUser);
 
       let user_follow_user_return = {
-        user_id_follower: client.userId,
+        user_id_follower: userId,
         user_id_followed: id,
         action: "in",
         source: 'onboarding',
@@ -56,12 +56,13 @@ const followUsers = async (token, userIds) => {
       const follows = [];
       userIds.map((item) => {
         follows.push({
-          source: "main_feed:" + client.userId,
+          source: "main_feed:" + userId,
           target: "user:" + item.toLowerCase(),
         });
       });
 
       const res = await clientServer.followMany(follows);
+      console.log('follow many: ', res);
       return res;
     })
     return result;
