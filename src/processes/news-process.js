9381927197a494255c991a197f79bf6e6dcd3329
usupsuparma <usup.suparma7@gmail.com>
2022-06-01 22:23:20 +0700

    fix: add validation remove subdomain

--- src/processes/news-process.js
+++ src/processes/news-process.js
@@ -1,210 +1,210 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const { DomainPage, NewsLink } = require("../databases/models");
-const { dateCreted, updateDomainCredderScore } = require("../utils");
+const { dateCreted, updateDomainCredderScore, removeSubDomain } = require("../utils");
 const { v4: uuidv4 } = require("uuid");
 const { credderScoreQueue } = require('../config');
 
 const validateDomain = async (resp) => {
-  try {
-    const removeWww = resp.request.host.replace("www.", "");
-    console.info('validate domain: ', removeWww);
-    const getDomain = await DomainPage.findOne({
-      where: { domain_name: removeWww }
-    })
+    try {
+        console.log('host', resp.request.host);
+        let removeWww = resp.request.host.replace("www.", "");
+        removeWww = removeSubDomain(removeWww);
+        const getDomain = await DomainPage.findOne({
+            where: { domain_name: removeWww }
+        })
 
-    let domain_id;
-    let name;
-    let info;
-    let domain_image;
-    let created_domain;
-    if (getDomain) {
-      domain_id = getDomain.dataValues.domain_page_id;
-      name = getDomain.dataValues.domain_name;
-      info = getDomain.dataValues.description;
-      domain_image = getDomain.dataValues.logo;
-      created_domain = getDomain.dataValues.created_at;
-    } else {
-      console.log(`${resp.request.protocol}//${resp.request.host}`);
-      const crawls = await axios.get(`${resp.request.protocol}//${resp.request.host}`,
-        { headers: { 'User-Agent': 'bettersocial' } });
-      const $ = cheerio.load(crawls.data);
-      const logo = $('meta[property="og:image"]').attr('content') || "";
-      const description = $('meta[property="og:description"]').attr('content') || "";
-      const { v4: uuidv4 } = require('uuid');
-      const domainId = uuidv4();
-      const data = {
-        domain_page_id: domainId,
-        domain_name: removeWww,
-        short_description: description.length > 254 ? description.substring(0, 230) : description,
-        logo,
-        ...dateCreted
-      }
+        let domain_id;
+        let name;
+        let info;
+        let domain_image;
+        let created_domain;
+        if (getDomain) {
+            domain_id = getDomain.dataValues.domain_page_id;
+            name = getDomain.dataValues.domain_name;
+            info = getDomain.dataValues.description;
+            domain_image = getDomain.dataValues.logo;
+            created_domain = getDomain.dataValues.created_at;
+        } else {
+            console.log(`${resp.request.protocol}//${resp.request.host}`);
+            const crawls = await axios.get(`${resp.request.protocol}//${resp.request.host}`,
+                { headers: { 'User-Agent': 'bettersocial' } });
+            const $ = cheerio.load(crawls.data);
+            const logo = $('meta[property="og:image"]').attr('content') || "";
+            const description = $('meta[property="og:description"]').attr('content') || "";
+            const { v4: uuidv4 } = require('uuid');
+            const domainId = uuidv4();
+            const data = {
+                domain_page_id: domainId,
+                domain_name: removeWww,
+                short_description: description.length > 254 ? description.substring(0, 230) : description,
+                logo,
+                ...dateCreted
+            }
 
-      await DomainPage.create(data);
+            await DomainPage.create(data);
 
-      domain_id = data.domain_page_id;
-      name = data.domain_name;
-      info = data.description;
-      domain_image = data.logo;
-      created_domain = data.created_at;
+            domain_id = data.domain_page_id;
+            name = data.domain_name;
+            info = data.description;
+            domain_image = data.logo;
+            created_domain = data.created_at;
+        }
+
+        const queueOptions = {
+            limiter: {
+                max: 300,
+                duration: 60 * 1000 //60k ms = 1 minute
+            }
+        }
+
+        console.log(`adding credder score queue ${removeWww}`)
+        credderScoreQueue.add({
+            domainName: removeWww
+        }, queueOptions)
+
+        return { domain_id, name, info, domain_image, created_domain }
+    } catch (error) {
+        console.log(error);
+        return error
     }
-    
-    const queueOptions = {
-      limiter: {
-        max: 300,
-        duration: 60 * 1000 //60k ms = 1 minute
-      }
-    }
-    
-    console.log(`adding credder score queue ${removeWww}`)
-    credderScoreQueue.add({
-      domainName: removeWww
-    }, queueOptions)
-
-    return { domain_id, name, info, domain_image, created_domain }
-  } catch (error) {
-    console.log(error);
-    return error
-  }
 }
 
 const putMainFeed = async (job, name, logo, created, data) => {
-  const { putStream } = require('../services');
+    const { putStream } = require('../services');
 
-  try {
-    const set = {
-      post_type: 2,
-      og: {
-        domain: name,
-        date: created,
-        domainImage: logo,
-        title: data.title,
-        description: data.description,
-        image: data.image,
-        url: data.news_url,
-        domain_page_id: data.domain_page_id,
-        news_link_id: data.news_link_id
-      }
+    try {
+        const set = {
+            post_type: 2,
+            og: {
+                domain: name,
+                date: created,
+                domainImage: logo,
+                title: data.title,
+                description: data.description,
+                image: data.image,
+                url: data.news_url,
+                domain_page_id: data.domain_page_id,
+                news_link_id: data.news_link_id
+            }
+        }
+        await putStream(job.id_feed, set);
+        console.info(`updated main_feed:${job.id_feed}`)
+    } catch (error) {
+        console.info(error)
     }
-    await putStream(job.id_feed, set);
-    console.info(`updated main_feed:${job.id_feed}`)
-  } catch (error) {
-    console.info(error)
-  }
 }
 
 const saveNewsLink = async (data, name, info, job, logo, created_domain) => {
-  const { v4: uuidv4 } = require('uuid');
-  const { postToGetstream } = require('./domain-process');
-  console.info('url: ', data.news_url);
-  try {
-    const findNewsLink = await NewsLink.findOne({
-      where: { news_url: data.news_url }
-    })
-    let message
-    if (findNewsLink) {
-      console.info("id", findNewsLink.news_link_id);
-      data.news_link_id = findNewsLink.news_link_id;
-      data.url = data.news_url;
+    const { v4: uuidv4 } = require('uuid');
+    const { postToGetstream } = require('./domain-process');
+    console.info('url: ', data.news_url);
+    try {
+        const findNewsLink = await NewsLink.findOne({
+            where: { news_url: data.news_url }
+        })
+        let message
+        if (findNewsLink) {
+            console.info("id", findNewsLink.news_link_id);
+            data.news_link_id = findNewsLink.news_link_id;
+            data.url = data.news_url;
 
-      const site_name = data.site_name
-      const activity = {
-        domain: {
-          name, site_name, info, image: logo, domain_page_id: data.domain_page_id
-        },
-        content: { ...data, ...dateCreted }
-      }
+            const site_name = data.site_name
+            const activity = {
+                domain: {
+                    name, site_name, info, image: logo, domain_page_id: data.domain_page_id
+                },
+                content: { ...data, ...dateCreted }
+            }
 
-      await putMainFeed(job, name, logo, created_domain, data);
-      // await postToGetstream(activity, job.user_id);
-      message = 'url news not unique'
-    } else {
+            await putMainFeed(job, name, logo, created_domain, data);
+            // await postToGetstream(activity, job.user_id);
+            message = 'url news not unique'
+        } else {
 
-      data.news_link_id = uuidv4();
-      data.url = data.news_url;
+            data.news_link_id = uuidv4();
+            data.url = data.news_url;
 
-      const site_name = data.site_name
-      const activity = {
-        domain: {
-          name, site_name, info, image: logo, domain_page_id: data.domain_page_id
-        },
-        content: { ...data, ...dateCreted }
-      }
+            const site_name = data.site_name
+            const activity = {
+                domain: {
+                    name, site_name, info, image: logo, domain_page_id: data.domain_page_id
+                },
+                content: { ...data, ...dateCreted }
+            }
 
-      await putMainFeed(job, name, logo, created_domain, data);
-      let result = await postToGetstream(activity, job.user_id);
+            await putMainFeed(job, name, logo, created_domain, data);
+            let result = await postToGetstream(activity, job.user_id);
 
-      let postId = result.returnActivityId
-      console.info(result);
-      console.info(`postId in Process : ${postId}`);
+            let postId = result.returnActivityId
+            console.info(result);
+            console.info(`postId in Process : ${postId}`);
 
-      await NewsLink.create({ ...data, ...dateCreted, post_id: result.id })
-      message = 'news link created'
+            await NewsLink.create({ ...data, ...dateCreted, post_id: result.id })
+            message = 'news link created'
+        }
+
+        return message
+    } catch (error) {
+        return error
     }
-
-    return message
-  } catch (error) {
-    return error
-  }
 }
 
 const newsJob = async (job, done) => {
-  try {
-    console.info('running job news! with id ' + job.id);
-    /*
-      @description crawls data from url post getstream
-    */
-    console.info('domain: ', job.data.body);
-    const crawls = await axios.get(job.data.body, { headers: { 'User-Agent': 'bettersocial' } });
+    try {
+        console.info('running job news! with id ' + job.id);
+        /*
+          @description crawls data from url post getstream
+        */
+        console.info('domain: ', job.data.body);
+        const crawls = await axios.get(job.data.body, { headers: { 'User-Agent': 'bettersocial' } });
 
-    /*
-      @description validate domain if exist get data or empty create data domain to table domain
-    */
-    const getDomain = await validateDomain(crawls);
-    const domain_page_id = getDomain.domain_id;
-    const name = getDomain.name;
-    const info = getDomain.info;
-    const logo = getDomain.domain_image;
-    const created_domain = getDomain.created_domain;
+        /*
+          @description validate domain if exist get data or empty create data domain to table domain
+        */
+        const getDomain = await validateDomain(crawls);
+        const domain_page_id = getDomain.domain_id;
+        const name = getDomain.name;
+        const info = getDomain.info;
+        const logo = getDomain.domain_image;
+        const created_domain = getDomain.created_domain;
 
-    /*
-      @description crawls attribut opengraph url domain
-    */
-    const $ = cheerio.load(crawls.data);
-    const site_name = $('meta[property="og:site_name"]').attr('content') || "";
-    const title = $('meta[property="og:title"]').attr('content') || "";
-    const image = $('meta[property="og:image"]').attr('content') || "";
-    let description = $('meta[property="og:description"]').attr('content') || "";
-    // if (description === "") {
-    //   description = $('meta[name="description"]').attr('content') || "";
-    // }
-    const news_url = $('meta[property="og:url"]').attr('content') || "";
-    const url_compact = $('meta[property="og:url"]').attr('content') || "";
-    const keyword = $('meta[name="keywords"]').attr('content') || "";
-    const author = $('meta[name="author"]').attr('content') || "";
+        /*
+          @description crawls attribut opengraph url domain
+        */
+        const $ = cheerio.load(crawls.data);
+        const site_name = $('meta[property="og:site_name"]').attr('content') || "";
+        const title = $('meta[property="og:title"]').attr('content') || "";
+        const image = $('meta[property="og:image"]').attr('content') || "";
+        let description = $('meta[property="og:description"]').attr('content') || "";
+        // if (description === "") {
+        //   description = $('meta[name="description"]').attr('content') || "";
+        // }
+        const news_url = $('meta[property="og:url"]').attr('content') || "";
+        const url_compact = $('meta[property="og:url"]').attr('content') || "";
+        const keyword = $('meta[name="keywords"]').attr('content') || "";
+        const author = $('meta[name="author"]').attr('content') || "";
 
-    /*
-      @description define data for post to getstream and newslink table
-    */
-    let data = {
-      domain_page_id, title, site_name, image, description, news_url, keyword, author, url_compact
-    };
+        /*
+          @description define data for post to getstream and newslink table
+        */
+        let data = {
+            domain_page_id, title, site_name, image, description, news_url, keyword, author, url_compact
+        };
 
 
-    /*
-      @description save news link and post to getstream
-    */
-    const result = await saveNewsLink(data, name, info, job.data, logo, created_domain);
-    console.info(result);
-    done(null, result);
-  } catch (error) {
-    console.log('kesalahan', error);
-    done(null, error);
-  }
+        /*
+          @description save news link and post to getstream
+        */
+        const result = await saveNewsLink(data, name, info, job.data, logo, created_domain);
+        done(null, result);
+    } catch (error) {
+        console.log('kesalahan', error);
+        done(null, error);
+    }
 }
 
 module.exports = {
-  newsJob,
-  // validateDomain
+    newsJob,
+    // validateDomain
 };
--- src/utils/custom.js
+++ src/utils/custom.js
@@ -1,126 +1,139 @@
 const convertString = (str, from, to) => {
-  return str.split(from).join(to);
+    return str.split(from).join(to);
 };
 
 const checkIfValidURL = (str) => {
-  try {
+    try {
 
 
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-    const isDomainOnly = !!pattern.test(str);
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        const isDomainOnly = !!pattern.test(str);
 
-    const urlRegex = /(https?:\/\/[^ ]*)/;
-    const urlValidation = str.match(urlRegex);
+        const urlRegex = /(https?:\/\/[^ ]*)/;
+        const urlValidation = str.match(urlRegex);
 
-    if (urlValidation) {
-      return str.match(urlRegex)[1];
-    } else if (isDomainOnly) {
-      const newStr = `https://${str}`;
-      return newStr.match(urlRegex)[1];
-    } else {
-      return false;
+        if (urlValidation) {
+            return str.match(urlRegex)[1];
+        } else if (isDomainOnly) {
+            const newStr = `https://${str}`;
+            return newStr.match(urlRegex)[1];
+        } else {
+            return false;
+        }
+        // const urlRegex = /(https?:\/\/[^ ]*)/;
+        // const urlValidation = str.match(urlRegex);
+
+        // if (urlValidation) {
+        //   return str.match(urlRegex)[1]
+        // } else {
+        //   return false
+        // }
+    } catch (error) {
+        console.log('error validation url', error);
+        return false;
     }
-    // const urlRegex = /(https?:\/\/[^ ]*)/;
-    // const urlValidation = str.match(urlRegex);
-
-    // if (urlValidation) {
-    //   return str.match(urlRegex)[1]
-    // } else {
-    //   return false
-    // }
-  } catch (error) {
-    console.log('error validation url', error);
-    return false;
-  }
 };
 
 
 
 const dateCreted = {
-  created_at: new Date().toISOString(),
-  updated_at: new Date().toISOString(),
+    created_at: new Date().toISOString(),
+    updated_at: new Date().toISOString(),
 };
 
 const getToken = (req) => {
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  } else {
-    token = null;
-  }
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    } else {
+        token = null;
+    }
 
-  return token;
+    return token;
 };
 
 function capitalizing(str) {
-  return (
-    str
-      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
-        return index == 0 ? word.toLowerCase() : word.toUpperCase();
-      })
-      // uppercase the first character
-      .replace(/^./, function (str) {
-        return str.toUpperCase();
-      })
-  );
+    return (
+        str
+            .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+                return index == 0 ? word.toLowerCase() : word.toUpperCase();
+            })
+            // uppercase the first character
+            .replace(/^./, function (str) {
+                return str.toUpperCase();
+            })
+    );
 }
 
 const generateRandomId = (a = '') =>
-  a
-    ? /* eslint-disable no-bitwise */
-    ((Number(a) ^ (Math.random() * 16)) >> (Number(a) / 4)).toString(16)
-    : `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, generateRandomId);
+    a
+        ? /* eslint-disable no-bitwise */
+        ((Number(a) ^ (Math.random() * 16)) >> (Number(a) / 4)).toString(16)
+        : `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, generateRandomId);
 
 const isStringBlankOrNull = (str) => {
-  if (str) {
-    if (str !== "") {
-      return false;
+    if (str) {
+        if (str !== "") {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 const countWordsWithoutLink = (str) => {
-  //console.debug("The sentence: " + str);
+    //console.debug("The sentence: " + str);
 
-  const filteredStr = str.replace(/(https?:\/\/[^ ]*)/, "");
-  //console.log("filtered:" + filteredStr);
+    const filteredStr = str.replace(/(https?:\/\/[^ ]*)/, "");
+    //console.log("filtered:" + filteredStr);
 
-  let matches = filteredStr.match(/([\w\d]+)/g);
+    let matches = filteredStr.match(/([\w\d]+)/g);
 
-  if (matches) {
-    return matches.length;
-  }
+    if (matches) {
+        return matches.length;
+    }
 
-  return 0;
+    return 0;
 }
 
 const countCharactersWithoutLink = (str) => {
-  //console.debug("The sentence: " + str);
+    //console.debug("The sentence: " + str);
 
-  const filteredStr = str.replace(/(https?:\/\/[^ ]*)/g, "");
-  //console.debug("filtered:" + filteredStr);
+    const filteredStr = str.replace(/(https?:\/\/[^ ]*)/g, "");
+    //console.debug("filtered:" + filteredStr);
 
-  //let chars = filteredStr.replace(/[^\a-zA-Z]/g,"");
-  return filteredStr.length;
+    //let chars = filteredStr.replace(/[^\a-zA-Z]/g,"");
+    return filteredStr.length;
+}
+const isSubDomain = (domain) => {// just for the example
+    var regex = new RegExp(/^([a-z]+\:\/{2})?([\w-]+\.[\w-]+\.\w+)$/);
+
+    return !!domain.match(regex); // make sure it returns boolean
+}
+const removeSubDomain = (domain) => {
+    if (isSubDomain(domain)) {
+        return domain.replace(/^[^.]+\./g, "");
+    } else {
+        return domain;
+    }
 }
 
 module.exports = {
-  convertString,
-  dateCreted,
-  getToken,
-  checkIfValidURL,
-  capitalizing,
-  generateRandomId,
-  isStringBlankOrNull,
-  countWordsWithoutLink,
-  countCharactersWithoutLink,
+    convertString,
+    dateCreted,
+    getToken,
+    checkIfValidURL,
+    capitalizing,
+    generateRandomId,
+    isStringBlankOrNull,
+    countWordsWithoutLink,
+    countCharactersWithoutLink,
+    removeSubDomain,
 };
