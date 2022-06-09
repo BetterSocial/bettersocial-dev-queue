a4fba591e4063a4f51e7f4803d4007d50253adec
usupsuparma <usup.suparma7@gmail.com>
2022-06-09 14:00:29 +0700

    fix: bugs prepopulated register

--- src/processes/register-process.js
+++ src/processes/register-process.js
@@ -1,173 +1,173 @@
 const StreamChat = require("stream-chat").StreamChat;
 const { convertString, capitalizing } = require("../utils/custom");
 const {
-  CHANNEL_TYPE_TOPIC,
-  CHANNEL_TYPE_GROUP_LOCATION,
-  ICON_TOPIC_CHANNEL,
-  ICON_LOCATION_CHANNEL,
+    CHANNEL_TYPE_TOPIC,
+    CHANNEL_TYPE_GROUP_LOCATION,
+    ICON_TOPIC_CHANNEL,
+    ICON_LOCATION_CHANNEL,
 } = require("../utils/constant");
 const prepopulated = require("../services/chat/prepopulated");
 const UserService = require("../services/postgres/UserService");
 const { followTopics, followUsers, followLocations } = require("../services");
 
 const addUserToChannel = async (userId, channelIds) => {
-  try {
-    const serverClient = StreamChat.getInstance(
-      process.env.API_KEY,
-      process.env.SECRET
-    );
-    channelIds.map(async (item) => {
-      const token = serverClient.createToken(userId);
-      const channelId = item;
-      const members = [];
-      members.push(userId);
+    try {
+        const serverClient = StreamChat.getInstance(
+            process.env.API_KEY,
+            process.env.SECRET
+        );
+        channelIds.map(async (item) => {
+            const token = serverClient.createToken(userId);
+            const channelId = item;
+            const members = [];
+            members.push(userId);
 
-      let name = convertString(channelId, "-", " ");
-      let channelName = capitalizing(name);
+            let name = convertString(channelId, "-", " ");
+            let channelName = capitalizing(name);
 
-      const channel = serverClient.channel("messaging", channelId, {
-        name: channelName,
-        created_by_id: "system",
-        channel_type: CHANNEL_TYPE_GROUP_LOCATION,
-        channelImage: ICON_LOCATION_CHANNEL,
-        channel_image: ICON_LOCATION_CHANNEL,
-        image: ICON_LOCATION_CHANNEL
-      });
-      await channel.create();
-      await channel.addMembers(members, {
-        text: "A new user has joined the group.",
-        user_id: userId,
-      });
-    });
-    console.info('addUser to channel: ', 'done');
-  } catch (error) {
-    console.log('error addUserToChannel: ', error);
-  }
+            const channel = serverClient.channel("messaging", channelId, {
+                name: channelName,
+                created_by_id: "system",
+                channel_type: CHANNEL_TYPE_GROUP_LOCATION,
+                channelImage: ICON_LOCATION_CHANNEL,
+                channel_image: ICON_LOCATION_CHANNEL,
+                image: ICON_LOCATION_CHANNEL
+            });
+            await channel.create();
+            await channel.addMembers(members, {
+                text: "A new user has joined the group.",
+                user_id: userId,
+            });
+        });
+        console.info('addUser to channel: ', 'done');
+    } catch (error) {
+        console.log('error addUserToChannel: ', error);
+    }
 };
 
 const addUserToTopicChannel = async (user_id, topics) => {
-  try {
-    let userId = user_id.toString();
-    const serverClient = StreamChat.getInstance(
-      process.env.API_KEY,
-      process.env.SECRET
-    );
-    topics.map(async (item) => {
-      const token = serverClient.createToken(userId);
-      const channelId = "topic_" + item;
-      const members = [];
-      members.push(userId);
+    try {
+        let userId = user_id.toString();
+        const serverClient = StreamChat.getInstance(
+            process.env.API_KEY,
+            process.env.SECRET
+        );
+        topics.map(async (item) => {
+            const token = serverClient.createToken(userId);
+            const channelId = "topic_" + item;
+            const members = [];
+            members.push(userId);
 
-      let name = capitalizing(item);
-      let channelName = "#" + convertString(name, "-", "");
+            let name = capitalizing(item);
+            let channelName = "#" + convertString(name, "-", "");
 
-      const channel = serverClient.channel("messaging", channelId, {
-        name: channelName,
-        created_by_id: "system",
-        channel_type: CHANNEL_TYPE_TOPIC,
-        channelImage: ICON_TOPIC_CHANNEL,
-        channel_image: ICON_TOPIC_CHANNEL,
-        image: ICON_TOPIC_CHANNEL
-      });
-      await channel.create();
+            const channel = serverClient.channel("messaging", channelId, {
+                name: channelName,
+                created_by_id: "system",
+                channel_type: CHANNEL_TYPE_TOPIC,
+                channelImage: ICON_TOPIC_CHANNEL,
+                channel_image: ICON_TOPIC_CHANNEL,
+                image: ICON_TOPIC_CHANNEL
+            });
+            await channel.create();
 
-      await channel.addMembers(members, {
-        text: "A new user has joined the group.",
-        user_id: userId,
-      });
-    });
-    console.log('addUserToTopicChannel', 'done');
-  } catch (error) {
-    console.log('addUserToTopicChannel: ', error);
-  }
+            await channel.addMembers(members, {
+                text: "A new user has joined the group.",
+                user_id: userId,
+            });
+        });
+        console.log('addUserToTopicChannel', 'done');
+    } catch (error) {
+        console.log('addUserToTopicChannel: ', error);
+    }
 };
 
 const prepopulatedDm = async (id, ids) => {
-  try {
-    let userService = new UserService();
-    // let userAdmin = await userService.getUserAdmin(process.env.USERNAME_ADMIN);
-    // let idAdmin = userAdmin.user_id;
-    // ids = ids.filter((element, i, ids) => {
-    //   return (element !== idAdmin);
-    // })
-    // ids.push(idAdmin);
-    let users = await userService.getUsersByIds(ids)
-    const pre = await prepopulated(id, users);
-    console.log('prepopulatedDm', 'done');
-  } catch (error) {
-    console.log('error prepopulatedDm: ', error);
-  }
+    try {
+        let userService = new UserService();
+        let userAdmin = await userService.getUserAdmin(process.env.USERNAME_ADMIN);
+        let idAdmin = userAdmin.user_id;
+        ids = ids.filter((element, i, ids) => {
+            return (element !== idAdmin);
+        })
+        ids.push(idAdmin);
+        let users = await userService.getUsersByIds(ids)
+        const pre = await prepopulated(id, users);
+        console.log('prepopulatedDm', 'done');
+    } catch (error) {
+        console.log('error prepopulatedDm: ', error);
+    }
 }
 
 
 const followLocation = async (userId, locations) => {
-  try {
-    let res = await followLocations(userId, locations);
-    console.log('followLocation', 'done');
-  } catch (error) {
-    console.log('followLocation: ', error);
-  }
+    try {
+        let res = await followLocations(userId, locations);
+        console.log('followLocation', 'done');
+    } catch (error) {
+        console.log('followLocation: ', error);
+    }
 };
 
 const followUser = async (userId, users) => {
-  try {
-    let res = await followUsers(userId, users);
-    // let userService = new UserService();
-    // let userAdmin = await userService.getUserAdmin(process.env.USERNAME_ADMIN);
-    // let idAdmin = userAdmin.user_id;
-    // users = users.filter((element, i, users) => {
-    //   return (element !== idAdmin);
-    // })
-    // users.push(idAdmin);
-    // let result = await userService.getUsersByIds(users);
-    // const pre = await prepopulated(id, result);
-    console.log('followUser', 'done');
-  } catch (error) {
-    console.log('followUser: ', error);
-  }
+    try {
+        let res = await followUsers(userId, users);
+        let userService = new UserService();
+        let userAdmin = await userService.getUserAdmin(process.env.USERNAME_ADMIN);
+        let idAdmin = userAdmin.user_id;
+        users = users.filter((element, i, users) => {
+            return (element !== idAdmin);
+        })
+        users.push(idAdmin);
+        let result = await userService.getUsersByIds(users);
+        const pre = await prepopulated(id, result);
+        console.log('followUser', 'done');
+    } catch (error) {
+        console.log('followUser: ', error);
+    }
 };
 
 const followTopic = async (userId, topics) => {
-  try {
-    let res = await followTopics(userId, topics);
-    console.log('followTopic', 'done');
-  } catch (error) {
-    console.log('followTopic: ', error);
-  }
+    try {
+        let res = await followTopics(userId, topics);
+        console.log('followTopic', 'done');
+    } catch (error) {
+        console.log('followTopic: ', error);
+    }
 };
 
 
 const registerProcess = async (job, done) => {
-  try {
-    console.info("running job register process ! with id " + job.id);
-    let data = job.data;
-    let { token,
-      userId,
-      locationsChannel,
-      follows,
-      topics,
-      locations } = data;
+    try {
+        console.info("running job register process ! with id " + job.id);
+        let data = job.data;
+        let { token,
+            userId,
+            locationsChannel,
+            follows,
+            topics,
+            locations } = data;
 
-    console.info('token: ', token);
-    console.log('userId: ', userId);
-    console.log('locationsChannel: ', locationsChannel);
-    console.log('follows: ', follows);
-    console.log('topics: ', topics);
-    console.log('locations: ', locations);
+        console.info('token: ', token);
+        console.log('userId: ', userId);
+        console.log('locationsChannel: ', locationsChannel);
+        console.log('follows: ', follows);
+        console.log('topics: ', topics);
+        console.log('locations: ', locations);
 
-    await addUserToChannel(userId, follows);
-    await addUserToTopicChannel(userId, topics);
-    await prepopulatedDm(userId, follows);
-    await followLocation(userId, locations);
-    await followUser(userId, follows);
-    await followTopic(userId, topics);
-    done(null, 'ok');
-  } catch (error) {
-    console.log(error);
-    done(null, error);
-  }
+        await addUserToChannel(userId, follows);
+        await addUserToTopicChannel(userId, topics);
+        await prepopulatedDm(userId, follows);
+        await followLocation(userId, locations);
+        await followUser(userId, follows);
+        await followTopic(userId, topics);
+        done(null, 'ok');
+    } catch (error) {
+        console.log(error);
+        done(null, error);
+    }
 }
 
 module.exports = {
-  registerProcess
+    registerProcess
 }
