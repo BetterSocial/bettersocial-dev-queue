5c1b010f1bada25e4a3220004334f91726fd70f7
usupsuparma <usup.suparma7@gmail.com>
2022-06-28 23:27:29 +0700

    fix: running all rss link

--- src/services/postgres/LinkRssService.js
+++ src/services/postgres/LinkRssService.js
@@ -1,44 +1,35 @@
 const { v4: uuidv4 } = require("uuid");
-const client = require('./pool');
-
-
+const client = require("./pool");
 
 const addLink = async ({ domain_name, link }) => {
-    const domain_page_id = uuidv4();
-    const createdAt = new Date().toISOString();
-    const query = {
-        text: 'INSERT INTO rss_links VALUES($1, $2, $3, $4, $5)',
-        values: [
-            domain_page_id,
-            domain_name,
-            link,
-            createdAt,
-            createdAt
-        ]
-    }
+  const domain_page_id = uuidv4();
+  const createdAt = new Date().toISOString();
+  const query = {
+    text: "INSERT INTO rss_links VALUES($1, $2, $3, $4, $5)",
+    values: [domain_page_id, domain_name, link, createdAt, createdAt],
+  };
 
-    const result = await client.query(query);
+  const result = await client.query(query);
 
-    if (!result.rowCount) {
-        // throw new InvariantError('Domain page gagal ditambahkan');
-        return null;
-    }
-    return true;
-}
+  if (!result.rowCount) {
+    // throw new InvariantError('Domain page gagal ditambahkan');
+    return null;
+  }
+  return true;
+};
 
 const getAllRssLinks = async () => {
-    const query = {
-        text: 'select * from rss_links order by random() limit 1',
-        values: []
-    }
+  const query = {
+    text: "select * from rss_links ",
+    values: [],
+  };
 
-    const result = await client.query(query);
+  const result = await client.query(query);
 
-    return result.rows[0];
-}
-
+  return result.rows;
+};
 
 module.exports = {
-    addLink,
-    getAllRssLinks
+  addLink,
+  getAllRssLinks,
 };
--- src/services/postgres/pool.js
+++ src/services/postgres/pool.js
@@ -1,14 +1,13 @@
-const { Pool, Client } = require('pg');
+const { Pool, Client } = require("pg");
 const client = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ?? 5432,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false
-    },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ?? 5432,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 
 module.exports = client;
