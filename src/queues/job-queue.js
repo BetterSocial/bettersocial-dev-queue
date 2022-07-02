3cfc0ef592df0d734e580c35d489d65ceea41a40
usupsuparma <usup.suparma7@gmail.com>
2022-07-02 21:45:44 +0700

    fix: getstream

--- src/queues/job-queue.js
+++ src/queues/job-queue.js
@@ -151,7 +151,7 @@ const initQueue = () => {
   );
 
   BetterSocialQueue.setEventCallback(dailyRssUpdateQueue, rssProcess);
-  BetterSocialQueue.setCron(dailyRssUpdateQueue, "43 14 * * *");
+  BetterSocialQueue.setCron(dailyRssUpdateQueue, "48 14 * * *");
 
   /**
    * (END) General Queue
