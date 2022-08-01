ef42230c82767b34a17495ab8d5245d287108ccb
usupsuparma <usup.suparma7@gmail.com>
2022-08-01 22:51:38 +0700

    fix: follow topic

--- src/services/followTopic.js
+++ src/services/followTopic.js
@@ -1,26 +1,26 @@
 const stream = require("getstream");
 const followTopic = async (token, userId) => {
-  let id = userId.toLowerCase();
-  const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
-  const user = client.feed("main_feed", client.userId, token);
-  return user.follow("topic", id);
+    let id = userId.toLowerCase();
+    const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
+    const user = client.feed("main_feed", client.userId, token);
+    return user.follow("topic", id);
 };
 
 const followTopics = async (userId, userIds) => {
-  // const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
-  const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
-  const follows = [];
-  userIds.map((item) => {
-    follows.push({
-      source: "main_feed:" + userId,
-      target: "topic:" + item.toLowerCase(),
+    // const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
+    const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
+    const follows = [];
+    userIds.map((item) => {
+        follows.push({
+            source: "main_feed:" + userId,
+            target: "topic:" + item,
+        });
     });
-  });
 
-  return await clientServer.followMany(follows);
+    return await clientServer.followMany(follows);
 };
 
 module.exports = {
-  followTopic,
-  followTopics,
+    followTopic,
+    followTopics,
 };
