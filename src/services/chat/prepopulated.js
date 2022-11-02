2ba4cdcb1afa7ba90aa009e9f3a0492e33a72a00
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2022-11-02 22:07:41 +0700

    fix: fix prepoluated

--- src/services/chat/prepopulated.js
+++ src/services/chat/prepopulated.js
@@ -65,24 +65,30 @@ module.exports = async (id, users) => {
              */
 
             const textOwnUser = `You started following ${user.username}. Send them a message now.`;
-            // await chat.addMembers({
-            //     text: textOwnUser,
-            //     user_id: id,
-            //     only_to_user_show: id,
-            //     disable_to_user: false,
-            //     channel_role: "channel_moderator",
-            //     is_add: true,
-            // });
+                        const textTargetUser = `${ownUser.username} started following you. Send them a message now`;
 
-            const textTargetUser = `${ownUser.username} started following you. Send them a message now`;
-            await chat.addMembers([user.user_id], {
-                text: user.user_id === ownUser.user_id ? textOwnUser : textTargetUser,
+            if(id === user.user_id) {
+                 await chat.addMembers([id],{
+                text: textOwnUser,
+                user_id: id,
+                only_to_user_show: id,
+                disable_to_user: false,
+                channel_role: "channel_moderator",
+                is_add: true,
+                });
+            } else {
+                 await chat.addMembers([user.user_id], {
+                text: textTargetUser,
                 user_id: user.user_id,
-                // only_to_user_show: false,
-                // disable_to_user: id,
+                only_to_user_show: false,
+                disable_to_user: id,
                 channel_role: "channel_moderator",
                 is_add: true
             });
+            }
+           
+
+           
             return status;
         });
 
