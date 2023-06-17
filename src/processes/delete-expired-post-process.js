ceacfbece59ad4cc143c29ca1e1511df0223ca88
falhar <firman.ali39@gmail.com>
2023-06-17 15:18:04 +0700

    feat: delete expired posts

--- src/processes/delete-expired-post-process.js
+++ src/processes/delete-expired-post-process.js
@@ -1,45 +1,55 @@
-const stream = require('getstream');
+const stream = require("getstream");
+const moment = require("moment");
 const { getDb } = require("../config/mongodb_conn");
-const {
-    DB_COLLECTION_POST_SCORE
-} = require("./scoring-constant");
+const { DB_COLLECTION_POST_SCORE } = require("./scoring-constant");
 const { deleteActivityProcessQueue } = require("../config");
+const { Posts } = require("../databases/models");
+const { Op } = require("sequelize");
 
 const deleteExpiredPostProcess = async (job, done) => {
-    success = true
+  success = true;
 
-    let date_ob = new Date();
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    let year = date_ob.getFullYear();
-    // Get list expired post from post_score
-    let expired_posts = {}
-    const db = await getDb();
-    await db.collection(DB_COLLECTION_POST_SCORE)
-        .find({ "expired_at": { "$lte": year + "-" + month + "-" + date, "$ne": "" } })
-        .toArray(async (err, docs) => {
-            docs.forEach(async doc => {
-                if (doc["author_id"] in expired_posts) {
-                    expired_posts[doc["author_id"]].push(doc["_id"])
-                } else {
-                    expired_posts[doc["author_id"]] = [doc["_id"]]
-                }
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  // Get list expired post from post_score
+  let expired_posts = {};
+  const db = await getDb();
+  await db
+    .collection(DB_COLLECTION_POST_SCORE)
+    .find({ expired_at: { $lte: year + "-" + month + "-" + date, $ne: "" } })
+    .toArray(async (err, docs) => {
+      docs.forEach(async (doc) => {
+        if (doc["author_id"] in expired_posts) {
+          expired_posts[doc["author_id"]].push(doc["_id"]);
+        } else {
+          expired_posts[doc["author_id"]] = [doc["_id"]];
+        }
+      });
+      if (expired_posts != {}) {
+        // Remove from getstream
+        for (const [key, value] of Object.entries(expired_posts)) {
+          value.forEach(async (act_id) => {
+            deleteActivityProcessQueue.add({
+              feedName: "user_excl",
+              userId: key,
+              activityId: act_id,
             });
-            if (expired_posts != {}) {
-                // Remove from getstream
-                for (const [key, value] of Object.entries(expired_posts)) {
-                    value.forEach(async act_id => {
-                        deleteActivityProcessQueue.add({ feedName: "user_excl", userId: key, activityId: act_id });
-                        await db.collection(DB_COLLECTION_POST_SCORE).deleteOne({ _id: act_id })
-                    })
-                }
-            }
-        });;
-    if (success) return done(null, 'OK')
-    return done(null, 'false')
-
-}
+            await db
+              .collection(DB_COLLECTION_POST_SCORE)
+              .deleteOne({ _id: act_id });
+          });
+        }
+      }
+    });
+  await Posts.destroy({
+    where: { duration: { [Op.lte]: moment().utc().toISOString() } },
+  });
+  if (success) return done(null, "OK");
+  return done(null, "false");
+};
 
 module.exports = {
-    deleteExpiredPostProcess
-}
+  deleteExpiredPostProcess,
+};
