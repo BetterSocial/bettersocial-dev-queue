6fe03a28364fab0af01545b9eb3cbb19f7d054dc
Hafizh Abdillah <hafizh.abdillah@flash-coffee.com>
2023-06-20 18:56:31 +0700

    feat: fix sonarqube

--- src/processes/follow-main-feed-f2-process.js
+++ src/processes/follow-main-feed-f2-process.js
@@ -7,38 +7,7 @@ const processFollow = async (job, done) => {
   const targetUserId = data.data.targetUserId;
   // find userIds followed by each userId and targetedUserId
   // follow not followed user(s) by userId to getStream
-
-  const findFollowersByUserId = UserFollowUser.findAll({
-    where: {
-      user_id_follower: userId,
-    },
-    limit: 1000,
-  });
-
-  const findFollowersByTargetUserId = UserFollowUser.findAll({
-    where: {
-      user_id_follower: targetUserId,
-    },
-    limit: 1000,
-  });
-
-  const [followersByUserId, followersByTargetUserId] = await Promise.all([
-    findFollowersByUserId,
-    findFollowersByTargetUserId,
-  ]);
-
-  const followersIdByUserId = followersByUserId.map(
-    (el) => el.user_id_followed
-  );
-  const followersIdByTargetUserId = followersByTargetUserId.map(
-    (el) => el.user_id_followed
-  );
-
-  const idsToFollow = userIdsToProcessByMainFeedF2(
-    followersIdByUserId,
-    followersIdByTargetUserId
-  );
-
+  const idsToFollow = _findUserIdsToProcess(userId, targetUserId);
   await followMainFeedF2(userId, idsToFollow);
 
   done();
@@ -51,7 +20,24 @@ const processUnfollow = async (job, done) => {
   const targetUserId = data.data.targetUserId;
   // find userIds followed by each userId and targetedUserId
   // un follow not followed user(s) by userId to getStream
+  const idsToUnfollow = _findUserIdsToProcess(userId, targetUserId);
+  await unFollowMainFeedF2(userId, idsToUnfollow);
 
+  done();
+};
+
+const userIdsToProcessByMainFeedF2 = (
+  followersIdByUserId,
+  followersIdByTargetUserId
+) => {
+  return followersIdByTargetUserId.filter((id) => {
+    const findIdx = followersIdByUserId.findIndex((fid) => id === fid);
+    // user followed this user
+    return findIdx > -1 ? false : true;
+  });
+};
+
+const _findUserIdsToProcess = async (userId, targetUserId) => {
   const findFollowersByUserId = UserFollowUser.findAll({
     where: {
       user_id_follower: userId,
@@ -78,31 +64,15 @@ const processUnfollow = async (job, done) => {
     (el) => el.user_id_followed
   );
 
-  const idsToUnfollow = userIdsToProcessByMainFeedF2(
+  const idsToProcess = userIdsToProcessByMainFeedF2(
     followersIdByUserId,
     followersIdByTargetUserId
   );
 
-  await unFollowMainFeedF2(userId, idsToUnfollow);
-
-  done();
-}
-
-const userIdsToProcessByMainFeedF2 = (
-  followersIdByUserId,
-  followersIdByTargetUserId
-) => {
-  return followersIdByTargetUserId.filter((id) => {
-    const findIdx = followersIdByUserId.findIndex((fid) => id === fid);
-    // user followed this user
-    if (findIdx > -1) {
-      return false;
-    }
-    return true;
-  });
+  return idsToProcess;
 };
 
 module.exports = {
   processFollow,
-  processUnfollow
+  processUnfollow,
 };
