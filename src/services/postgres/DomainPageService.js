8eabccba5358cfcd18d917c09a745f5ce60ea0c4
falhar <firman.ali39@gmail.com>
2023-08-06 15:57:23 +0700

    fix: logo not show in news feed

--- src/services/postgres/DomainPageService.js
+++ src/services/postgres/DomainPageService.js
@@ -1,76 +1,76 @@
 const { v4: uuidv4 } = require("uuid");
-const client = require('./pool');
+const client = require("./pool");
 
 const getAllDomains = async () => {
-    const query = {
-        text: 'SELECT * FROM domain_page',
-        values: [],
-    };
+  const query = {
+    text: "SELECT * FROM domain_page",
+    values: [],
+  };
 
-    try {
-        let result = await client.query(query);
-        return result.rows;
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
-}
+  try {
+    let result = await client.query(query);
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
 
 const getDomainByDomainName = async (domainName) => {
-    const query = {
-        text: 'SELECT domain_page_id FROM domain_page WHERE domain_name = $1',
-        values: [domainName],
-    };
+  const query = {
+    text: "SELECT * FROM domain_page WHERE domain_name = $1",
+    values: [domainName],
+  };
 
-    const result = await client.query(query);
+  const result = await client.query(query);
 
-    if (!result.rows.length) {
-        return null;
-    }
-    return result.rows[0];
-}
+  if (!result.rows.length) {
+    return null;
+  }
+  return result.rows[0];
+};
 
 const addDomain = async ({ domain_name, logo, short_description }) => {
-    const domain_page_id = uuidv4();
-    const createdAt = new Date().toISOString();
-    const query = {
-        text: 'INSERT INTO domain_page VALUES($1, $2, $3, $4, $5, $6) RETURNING domain_page_id',
-        values: [
-            domain_page_id,
-            domain_name,
-            logo,
-            short_description,
-            createdAt,
-            createdAt
-        ]
-    }
+  const domain_page_id = uuidv4();
+  const createdAt = new Date().toISOString();
+  const query = {
+    text: "INSERT INTO domain_page VALUES($1, $2, $3, $4, $5, $6) RETURNING domain_page_id",
+    values: [
+      domain_page_id,
+      domain_name,
+      logo,
+      short_description,
+      createdAt,
+      createdAt,
+    ],
+  };
 
-    const result = await client.query(query);
+  const result = await client.query(query);
 
-    if (!result.rowCount) {
-        return null;
-    }
-    return result.rows[0].domain_page_id;
-}
+  if (!result.rowCount) {
+    return null;
+  }
+  return result.rows[0].domain_page_id;
+};
 
 const getDomainById = async (domainId) => {
-    const query = {
-        text: 'SELECT *  FROM domain_page WHERE id = $1',
-        values: [domainId],
-    };
+  const query = {
+    text: "SELECT *  FROM domain_page WHERE id = $1",
+    values: [domainId],
+  };
 
-    const result = await client.query(query);
+  const result = await client.query(query);
 
-    if (!result.rows.length) {
-        throw new NotFoundError('User tidak ditemukan');
-    }
+  if (!result.rows.length) {
+    throw new NotFoundError("User tidak ditemukan");
+  }
 
-    return result.rows[0];
-}
+  return result.rows[0];
+};
 
 module.exports = {
-    getAllDomains,
-    getDomainByDomainName,
-    addDomain,
-    getDomainById
+  getAllDomains,
+  getDomainByDomainName,
+  addDomain,
+  getDomainById,
 };
