b14c691828a1344b2f84cd0182b3e3f7fe2ce287
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-10 22:05:25 +0700

    chore: migrate redis

--- src/queues/queueSenderForRedis.js
+++ src/queues/queueSenderForRedis.js
@@ -1,33 +1,28 @@
-const Bull = require("bull");
-const { v4: uuidv4 } = require("uuid");
+const Bull = require('bull');
+const {v4: uuidv4} = require('uuid');
+
 const connectRedis = process.env.REDIS_ENTERPRISE_URL
   ? process.env.REDIS_ENTERPRISE_URL
-  : process.env.REDIS_TLS_URL;
+  : process.env.REDIS_ENTERPRISE_URL;
 
 // init the scoring process queue object, to be used on sending message to the queue
 // For production
 
 const redisOptions = {
   redis: {
-    tls: {
-      rejectUnauthorized: false,
-    },
+    // tls: {
+    //   rejectUnauthorized: false
+    // },
     maxRetriesPerRequest: 100,
-    connectTimeout: 30000,
-  },
+    connectTimeout: 30000
+  }
 };
 
 // For local
 // const redisOptions = {};
 
-const scoringDailyProcessQueue = new Bull(
-  "scoringDailyProcessQueue",
-  connectRedis,
-  redisOptions
-);
-scoringDailyProcessQueue.on("error", (err) =>
-  console.log("scoringDailyProcessQueue", err)
-);
+const scoringDailyProcessQueue = new Bull('scoringDailyProcessQueue', connectRedis, redisOptions);
+scoringDailyProcessQueue.on('error', (err) => console.log('scoringDailyProcessQueue', err));
 
 const sendQueueForDailyProcess = async (event, data) => {
   // console.log(
@@ -38,19 +33,17 @@ const sendQueueForDailyProcess = async (event, data) => {
   //     "]"
   // );
 
-  let queueData = {
-    event: event,
-    data: data,
+  const queueData = {
+    event,
+    data
   };
 
   const options = {
     jobId: uuidv4(),
-    removeOnComplete: true,
+    removeOnComplete: true
   };
 
   scoringDailyProcessQueue.add(queueData, options);
-
-  return;
 };
 
 const sendQueueForCronDailyProcess = async (event, data) => {
@@ -62,23 +55,23 @@ const sendQueueForCronDailyProcess = async (event, data) => {
   //     "]"
   // );
 
-  let queueData = {
-    event: event,
-    data: data,
+  const queueData = {
+    event,
+    data
   };
 
   const options = {
     jobId: uuidv4(),
-    removeOnComplete: true,
+    removeOnComplete: true
   };
 
   return await scoringDailyProcessQueue.add(queueData, {
     ...options,
-    backoff: { type: "exponential", delay: 5 * 1000 },
+    backoff: {type: 'exponential', delay: 5 * 1000}
   });
 };
 
 module.exports = {
   sendQueueForDailyProcess,
-  sendQueueForCronDailyProcess,
+  sendQueueForCronDailyProcess
 };
