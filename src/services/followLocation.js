824bb1c61a87da971659a17834c7637393502ac1
Nandang Permana <permana0415@gmail.com>
2023-08-18 17:17:27 +0700

    feat: update queue process register v2

--- src/services/followLocation.js
+++ src/services/followLocation.js
@@ -18,7 +18,7 @@ const followDefaultLocation = async(userId) => {
     const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
     const follows = [];
     follows.push({
-        source: "main_feed:" + userId,
+        source: "main_feed_following:" + userId,
         target: "location:everywhere",
     });
     return await clientServer.followMany(follows);
@@ -30,7 +30,7 @@ const followLocations = async (userId, locations) => {
     /* istanbul ignore next */
     locations.map((item) => {
         follows.push({
-            source: "main_feed:" + userId,
+            source: "main_feed_following:" + userId,
             target: "location:" + item,
         })
     });
