a4380f9cbc30b778a76f2a439641d1655f7af0e9
Nandang Permana <permana0415@gmail.com>
2023-08-20 16:04:19 +0700

    feat: reset and sync feed

--- src/routes/feedRoute.js
+++ src/routes/feedRoute.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const { refreshPostViewTime } = require("../services");
-const { syncFeedPerUser } = require("../services");
+const { syncFeedPerUser, unfollowFeed, getFeedFollowing, getFeedFollower, getFeedActivities, resetAndSyncFeed} = require("../services");
 
 const router = express.Router();
 
 router.get("/refresh-post-view-time", refreshPostViewTime);
 // sync user feed
 router.get("/sync-feed/:userId", syncFeedPerUser);
+router.post("/unfollow-feed", unfollowFeed);
+router.post("/feed-following", getFeedFollowing);
+router.post("/feed-follower", getFeedFollower);
+router.post("/feed-activities", getFeedActivities);
+router.post("/reset-sync-feed", resetAndSyncFeed);
+
+
 // sync all user feed
 // router.get("/sync-feed", refreshPostViewTime);
 
