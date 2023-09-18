c91a95b412d7fa584fb24a2590ff2614fa6a1e49
n-permana <permana0415@gmail.com>
2023-09-18 13:45:49 +0700

    feat: add get follower functionality

--- src/databases/functions/users/index.js
+++ src/databases/functions/users/index.js
@@ -1,9 +1,10 @@
 const UsersFunction = {
-    register: require('./users-register'),
-    registerAnonymous: require('./users-register-anonymous'),
-    getUserTopicList: require('./get-user-topic-list'),
-    getUserFollowingList: require('./get-user-following-list'),
-    getUserByUserId: require('./get-user-by-user-id')
-}
+  register: require("./users-register"),
+  registerAnonymous: require("./users-register-anonymous"),
+  getUserTopicList: require("./get-user-topic-list"),
+  getUserFollowingList: require("./get-user-following-list"),
+  getUserFollowerList: require("./get-user-follower-list"),
+  getUserByUserId: require("./get-user-by-user-id"),
+};
 
 module.exports = UsersFunction;
