fe51737da8b205cf9d7ff9424af10b1401757f26
brm-stnd <brm.stnd@gmail.com>
2023-10-12 23:04:32 +0700

    feat: ping-2866 test chat msg format

--- src/routes/newsRoute.js
+++ src/routes/newsRoute.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 
-const { createQueueNews } = require("../webhook/setNews");
-const { setTestAddUserPostComment } = require("../webhook/setTestAddUserPostComment");
-const { setTestDeleteUserPostComment } = require("../webhook/setTestDeleteUserPostComment");
-const { setTestRegisterV2 } = require("../webhook/setTestRegisterV2");
+const {createQueueNews} = require('../webhook/setNews');
+const {testMsgFormat} = require('../webhook/testMsgFormat');
+const {setTestAddUserPostComment} = require('../webhook/setTestAddUserPostComment');
+const {setTestDeleteUserPostComment} = require('../webhook/setTestDeleteUserPostComment');
+const {setTestRegisterV2} = require('../webhook/setTestRegisterV2');
 const router = express.Router();
 
-router.post("/message-posted", createQueueNews);
+router.post('/message-posted', createQueueNews);
+router.post('/test/message-format', testMsgFormat);
 // router.post("/test", serviceTestQueue);
 // router.post("/test-rss", testRss);
 // router.post("/test/credder", setTestCredder)
 // router.post("/test/add-user-post-comment", setTestAddUserPostComment)
 // router.post("/test/delete-user-post-comment", setTestDeleteUserPostComment)
-router.post("/test/register-v2", setTestRegisterV2)
+router.post('/test/register-v2', setTestRegisterV2);
 module.exports = router;
