8ce7a4620058701b2d3dfe4721b8da2c34b04146
brm-stnd <brm.stnd@gmail.com>
2023-12-14 14:18:38 +0700

    fix: update script for handle if already unfoll by users before

--- src/services/user/autoFollowUserAnon.js
+++ src/services/user/autoFollowUserAnon.js
@@ -1,6 +1,6 @@
 const {successResponse} = require('../../utils');
 const {v4: uuidv4} = require('uuid');
-const {User, UserFollowUser} = require('../../databases/models');
+const {UserFollowUser, UserFollowUserHistory} = require('../../databases/models');
 const UsersFunction = require('../../databases/functions/users');
 
 const autoFollowUserAnon = async (req, res) => {
@@ -12,30 +12,48 @@ const autoFollowUserAnon = async (req, res) => {
     });
 
     const userFollowerAnonData = [];
+    const userFollowerAnonDataHistory = [];
     await Promise.all(
       userFollowUserList.map(async (followUser) => {
         const anonId = await UsersFunction.findAnonymousUserId(followUser.user_id_followed);
 
-        const followAnonUser = await UserFollowUser.findOne({
-          where: {
-            user_id_follower: followUser.user_id_follower,
-            user_id_followed: anonId,
-            is_anonymous: true
-          }
-        });
+        const [followAnonUser, isUnfollowByUser] = await Promise.all([
+          UserFollowUser.findOne({
+            where: {
+              user_id_follower: followUser.user_id_follower,
+              user_id_followed: anonId,
+              is_anonymous: true
+            }
+          }),
+          UserFollowUserHistory.findOne({
+            where: {
+              user_id_follower: followUser.user_id_follower,
+              user_id_followed: anonId,
+              action: 'out'
+            }
+          })
+        ]);
 
-        if (!followAnonUser) {
+        if (!followAnonUser && !isUnfollowByUser) {
           userFollowerAnonData.push({
             follow_action_id: uuidv4(),
             user_id_follower: followUser.user_id_follower,
             user_id_followed: anonId,
             is_anonymous: true
           });
+
+          userFollowerAnonDataHistory.push({
+            user_id_follower: followUser.user_id_follower,
+            user_id_followed: anonId,
+            action: 'in',
+            source: `script`
+          });
         }
       })
     );
 
     let returnUserFollowUser = await UserFollowUser.bulkCreate(userFollowerAnonData);
+    await UserFollowUserHistory.bulkCreate(userFollowerAnonDataHistory);
 
     return successResponse(res, 'Success Follow Anon Users', returnUserFollowUser);
   } catch (error) {
