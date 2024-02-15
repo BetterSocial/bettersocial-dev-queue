7114f3b6a3126fad6cd8032cebf45776ee5e4111
brm-stnd <brm.stnd@gmail.com>
2024-02-15 20:01:39 +0700

    fix: ping-3605 fix channel register

--- src/services/followMainFeedFollowing.js
+++ src/services/followMainFeedFollowing.js
@@ -1,4 +1,5 @@
-const stream = require("getstream");
+const stream = require('getstream');
+const UserService = require('../services/postgres/UserService');
 
 const followMainFeedFollowing = async (userId, userIds) => {
   if (!userIds || userIds.length == 0) {
@@ -6,18 +7,19 @@ const followMainFeedFollowing = async (userId, userIds) => {
   }
 
   const cs = stream.connect(process.env.API_KEY, process.env.SECRET);
+  const userSevice = new UserService();
   const userAdmin = await userSevice.getUserAdmin(process.env.USERNAME_ADMIN);
 
   userIds.push(userAdmin.user_id);
   const payload = userIds.map((ui) => {
     return {
       source: `main_feed_following:${userId}`,
-      target: `user_excl:${ui}`,
+      target: `user_excl:${ui}`
     };
   });
   return await cs.followMany(payload);
 };
 
 module.exports = {
-  followMainFeedFollowing,
+  followMainFeedFollowing
 };
