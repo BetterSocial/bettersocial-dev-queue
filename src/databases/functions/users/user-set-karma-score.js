f8ccaf871457be4944d9d2a45cadd3d773a19698
brm-stnd <brm.stnd@gmail.com>
2024-03-27 15:52:44 +0700

    feat: update karma score with follower count

--- src/databases/functions/users/user-set-karma-score.js
+++ src/databases/functions/users/user-set-karma-score.js
@@ -1,10 +1,34 @@
 const {User} = require('../../models');
+const {QueryTypes} = require('sequelize');
+const db = require('../../models/');
 
 module.exports = async (userId, combined_user_score, karma_score) => {
   if (!userId) return null;
   try {
+    let total_follower = 0;
+    const queryTotalFollower = `
+      SELECT user_id_followed, COUNT(user_id_follower) AS follower_count
+      FROM user_follow_user
+      WHERE
+      is_anonymous = false
+      AND user_id_followed = :userId
+      GROUP BY user_id_followed
+    `;
+
+    const resultTotalFollower = await db.sequelize.query(queryTotalFollower, {
+      type: QueryTypes.SELECT,
+      replacements: {
+        userId
+      },
+      raw: true
+    });
+
+    if (queryTotalFollower) {
+      total_follower = resultTotalFollower[0]?.follower_count;
+    }
+
     const result = await User.update(
-      {combined_user_score, karma_score},
+      {combined_user_score, karma_score, followers_count: total_follower},
       {where: {user_id: userId}}
     );
     return result;
